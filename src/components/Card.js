ed05087d9ff9ec318f2bc52f793f49d36cd2b2a2
Roni Koskinen <roni.koskinen@gmail.com>
2022-06-14 00:42:15 +0300

    first

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+
+
+const Card = (src) => { 
+    
+    return (
+        <>
+        <img className="mokkikuva" src={src.src} alt="" label={src.label} />
+        <p>{src.text}</p>
+        </>
+    )
+ }
+
+export default Card
