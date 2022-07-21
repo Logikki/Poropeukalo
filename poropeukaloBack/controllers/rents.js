fc712dd6ab47e260864a1d3d0a866745923ca3f7
Roni Koskinen <roni.koskinen@gmail.com>
2022-07-21 15:01:28 +0300

    lähettää sähköpostiin varausvahvistuksen

--- poropeukaloBack/controllers/rents.js
+++ poropeukaloBack/controllers/rents.js
@@ -1,7 +1,9 @@
-
+/* eslint-disable no-unused-vars */
 /* rentsRouter handles requests to url /api/rents */
 const rentsRouter = require('express').Router()
 const Rent = require('../models/rent')
+const nodeMailer = require('nodemailer')
+const config = require('../utils/config')
 
 /* Function to handle post request. */ 
 rentsRouter.post('/', async (req, res) => {
@@ -23,11 +25,35 @@ rentsRouter.post('/', async (req, res) => {
       email: body.email,
       lisatieto: body.lisatieto
     })
-
+  
     const savedRent = await rentinf.save()
-    res.status(201).json(savedRent)
-  }
-})
+    /** Sending email with all the data */
+    console.log(config.EMAIL)
+    let transporter = nodeMailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: config.EMAIL,
+        pass: config.PASS
+      }
+    })
+
+    let mailOptions = {
+      from: '"poropeukalo" <porobotti@gmail.com>', // sender address
+      to: body.email, //receiver address
+      subject: 'varausvahvistus', 
+      text: `Kiitos varauksestasi, varaus toteutettiin näillä tiedoilla: \n ${JSON.stringify(rentinf)}`, 
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error)
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response)
+      res.render('index')
+    })
+  }})
 
 /*Function handles get request. Return all the json data in the mongodb server  */ 
 rentsRouter.get('/', async (req,res) => {
--- poropeukaloBack/utils/config.js
+++ poropeukaloBack/utils/config.js
@@ -2,8 +2,13 @@ require('dotenv').config()
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+let EMAIL = process.env.EMAIL
+let PASS = process.env.PASS
+
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  EMAIL,
+  PASS
 }
